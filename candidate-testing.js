5208646d6eb0ef430d129f36974944f6aee22e82
aapuzzo <>
2021-05-12 18:10:26 +0000

    book tricked me, copy pasted expected from GitHub

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = [ "Who was the first American woman in space? ", "True or false: 5000 meters == 5 Kilometers. ", "(5 + 3)/2 * 10= ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the iss? " ];
+let questions = [ "Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? " ];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -38,11 +38,11 @@ function gradeQuiz(candidateAnswers) {
 
   let grade = (correctCount/questions.length)*100;
   if (grade >= 80){
-    console.log(`>>> Overall Grade: ${grade}% (${correctCount} of ${questions.length} correct) <<<`);
+    console.log(`>>> Overall Grade: ${grade}% (${correctCount} of responses ${questions.length} correct) <<<`);
     console.log(`>>> Status: PASS <<<`);
   } 
   else {
-    console.log(`>>> Overall Grade: ${grade}% (${correctCount} of ${questions.length} correct) <<<`);
+    console.log(`>>> Overall Grade: ${grade}% (${correctCount} of responses ${questions.length} correct) <<<`);
     console.log(`>>> Status: Failed <<<`);
   }
     
